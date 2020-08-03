a25a29390b23eab933a9f3fd6508a49b9a658bdd
kwe <kwevans@gmail.com>
2020-08-03 15:07:02 +0100

    getting started

---
+++ day1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+var age = 56
+
+var population = 8_000_000
+
+var str1 = """
+This is a very
+log string, that goes
+over multiple lines
+"""
+
+var str2 = """
+This is a very \
+log string, that goes \
+over multiple lines
+"""
+
+var pi = 3.141
+var awesome = true
+
+var score = 89
+var str3 = "Your score is \(score)"
+
+let taylor = "Swift"
+
+let album: String = "Folklore"
+let year:Int = 2020
+let height:Float = 1.78
+let taylorRocks: Bool = true
+
